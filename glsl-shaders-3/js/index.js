ebba3acac40b480ef86f16dd5f23249f04c71e2d
Viktor Kravchuk <9576633+rkuhcvark@users.noreply.github.com>
2024-02-10 14:00:20 +0200

    time changing colors

--- glsl-shaders-3/js/index.js
+++ glsl-shaders-3/js/index.js
@@ -13,10 +13,12 @@ const fshader = `
   uniform vec2 u_resolution;
 
   void main() {
+    vec2 v = u_mouse / u_resolution;
+
     vec3 color = vec3(
-      (sin(u_time) + 1.0) / 2.0,
+      v.x,
       0.0,
-      (cos(u_time) + 1.0) / 2.0
+      v.y
     );
 
     gl_FragColor = vec4(color, 1.0);
